306b3f22b7f4870ce837d2e59879738b723cde93
Vlad Kiperman <vkiperman@cars.com>
2015-03-18 09:20:28 -0500

    working on bid info

--- public/javascripts/bidinformation.js
+++ public/javascripts/bidinformation.js
@@ -2,7 +2,7 @@
 
 	var app = angular.module('BidInformation', []);
 
-	app.directive('bidInformation', function(){
+	app.directive('bidInformation', ['numberFilter', function(numberFilter){
 
 		return {
 			restrict: 'E',
@@ -10,6 +10,61 @@
 
 			controller: function($scope){
 				$scope.currentTab = 0;
+
+				$scope.vehicleInfo = [
+					{
+						name: 'Series', value: 'Limited'
+					},
+					{
+						name: 'Body Style', value: '4D Utility'
+					},
+					{
+						name: 'Exterior Color', value: 'Arctic Frost'
+					},
+					{
+						name: 'Interior Type', value: 'Cloth'
+					},
+					{
+						name: 'Interior Color', value: 'Charcoal'
+					},
+					{
+						name: 'VIN', value: '19WWA5641XA011701'
+					}
+				];
+				$scope.conditionSummary = [
+					{
+						name: 'Damage', value: 'None'
+					},
+					{
+						name: 'Title', value: 'Clear'
+					},
+					{
+						name: 'Smoker', value: 'No'
+					},
+					{
+						name: 'Odometer', value: '46,154'
+					},
+					{
+						name: 'Operable', value: 'Yes'
+					},
+				];
+				$scope.customerInfo = [
+					{
+						name: 'Name', value: 'Limited'
+					},
+					{
+						name: 'City, State, ZIP', value: 'Chicago, IL, 60505'
+					},
+					{
+						name: 'Email', value: 'myName@gmail.com'
+					},
+					{
+						name: 'Phone', value: '312-245-7879'
+					},
+					{
+						name: 'Need Replacement Vehicle', value: 'No'
+					}
+				];
 
 				$scope.setTab = function(currentTab){
 					if($scope.currentTab !== currentTab){
@@ -24,6 +79,6 @@
 
 		};
 
-	});
+	}]);
 
 })();
