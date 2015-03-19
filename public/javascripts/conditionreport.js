a29c3ece2e646bc855c0a8f6966acee01e056c48
Vlad Kiperman <vkiperman@cars.com>
2015-03-19 11:23:19 -0500

    working on bid info

--- public/javascripts/conditionreport.js
+++ public/javascripts/conditionreport.js
@@ -9,6 +9,20 @@
 			templateUrl: '/includes/condition-report.html',
 
 			controller: function($scope){
+				$scope.vehicleHistory = [
+					{name:'Status of title', value:'Clear'},
+					{name:'Title location', value:'I have the title'},
+					{name:'Is your vehicle operable?', value:'Yes'},
+					{name:'Has your car been in an accident?', value:'No'},
+					{name:'Have after-market parts been added or removed?', value:'No'},
+					{name:'Has your car had previous paint or body work done?', value:'No'}
+				];
+				$scope.exterior = [
+					{name:'Paint damage', value:'Chips'},
+					{name:'Body damage', value:'No'},
+					{name:'Glass damage', value:'No'},
+					{name:'Lights damage', value:'No'}
+				];
 
 			}
 		};
