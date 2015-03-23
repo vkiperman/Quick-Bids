3cf931c37199642c783ec379b87822ed908c4da2
Vlad Kiperman <vkiperman@cars.com>
2015-03-23 06:32:51 -0500

    added link, controller

--- public/javascripts/viewphotos.js
+++ public/javascripts/viewphotos.js
@@ -6,7 +6,22 @@
 
 		return {
 			restrict: 'E',
-			templateUrl: '/includes/view-photos.html'
+			templateUrl: '/includes/view-photos.html',
+
+			link: function(scope, element, attrs, obj){
+				
+			},
+
+			controller: function($scope){
+
+				$scope.cancelManager = function(event){
+					$scope.$parent.cancelManager(event);
+				};
+
+				$scope.showImage = function(href){
+					window.open(href);
+				};
+			}
 		};
 
 	}]);
