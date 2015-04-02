0dbda8a4ab635a95ff98c02acd74619a48fc199d
Vlad Kiperman <vkiperman@cars.com>
2015-04-02 15:56:40 -0500

    added animation

--- temp/directive.js
+++ temp/directive.js
@@ -5,10 +5,20 @@
 	app.directive('%DIRECTIVENAME%', function(){
 
 		return {
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change
+			// controller: function($scope, $element, $attrs, $transclude) {},
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			// template: '',
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 			restrict: 'E',
 			templateUrl: '/includes/%TEMPLATENAME%',
 
-			link: function(scope, element, attrs){
+			link: function(scope/*, element, attrs, transclude*/){
 
 			},
 
