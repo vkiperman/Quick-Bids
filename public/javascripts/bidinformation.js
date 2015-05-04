abcbde6991f9a3b5fef70ed94cc9a1dcf577317c
Vlad Kiperman <vkiperman@cars.com>
2015-05-04 15:30:49 -0500

    converted content to directives

--- public/javascripts/bidinformation.js
+++ public/javascripts/bidinformation.js
@@ -1,6 +1,12 @@
 (function(){
 
-	var app = angular.module('BidInformation', []);
+	var app = angular.module('BidInformation', [
+		'Vehicleinfo',
+		'Conditionsummary',
+		'Vehicleoptions',
+		'Customerdetail',
+		'Externalresources'
+	]);
 
 	app.directive('bidInformation', ['numberFilter', function(numberFilter){
 
@@ -8,76 +14,11 @@
 			restrict: 'E',
 			templateUrl: '/includes/bid-information.html',
 
-			scope: {/*currentTab: '=currentTab'*/}, 
+			scope: {}, 
 
 			controller: function($scope){
+				$scope.heading = 'Bid Information';
 				$scope.verified = false;
-
-				$scope.vehicleInfo = [
-					{
-						name: 'Series', value: 'Limited', verify: 'Wrong'
-					},
-					{
-						name: 'Body Style', value: '4D Utility', verify: '4D Utility'
-					},
-					{
-						name: 'Exterior Color', value: 'Arctic Frost', verify: 'Arctic Frost'
-					},
-					{
-						name: 'Interior Type', value: 'Cloth', verify: 'Cloth'
-					},
-					{
-						name: 'Interior Color', value: 'Charcoal', verify: 'No'
-					},
-					{
-						name: 'VIN', value: '19WWA5641XA011701'
-					}
-				];
-				$scope.$parent.vehicleInfo = $scope.vehicleInfo;
-				$scope.conditionSummary = [
-					{
-						name: 'Damage', value: 'None', verify: 'None'
-					},
-					{
-						name: 'Title', value: 'Clear', verify: 'Clear'
-					},
-					{
-						name: 'Smoker', value: 'No', verify: 'Yes'
-					},
-					{
-						name: 'Odometer', value: '46,154', verify: '46,165'
-					},
-					{
-						name: 'Operable', value: 'Yes', verify: 'No'
-					},
-				];
-				$scope.customerInfo = [
-					{
-						name: 'Name', value: 'Limited'
-					},
-					{
-						name: 'City, State, ZIP', value: 'Chicago, IL, 60505'
-					},
-					{
-						name: 'Email', value: 'myName@gmail.com'
-					},
-					{
-						name: 'Phone', value: '312-245-7879'
-					},
-					{
-						name: 'Need Replacement Vehicle?', value: 'No'
-					}
-				];
-
-				/*$scope.setTab = function(currentTab){
-					if($scope.currentTab !== currentTab){
-						$scope.currentTab = currentTab;
-					}
-				};
-
-				$scope.isCurrentTab = function(match){
-					return $scope.currentTab === match;
-				}*/
 			}
 
 		};
