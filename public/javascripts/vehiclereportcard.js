e8263628246da956167043aa5f56791592e03eda
Vlad Kiperman <vkiperman@cars.com>
2015-05-08 15:16:52 -0500

    removed superfluous code

--- public/javascripts/vehiclereportcard.js
+++ public/javascripts/vehiclereportcard.js
@@ -6,22 +6,6 @@
 		return {
 			restrict: 'E',
 			templateUrl: '/includes/vehicle-report-card.html',
-
-			scope: {currentTab: '=currentTab'},
-
-			controller: function($scope){
-				$scope.totalReconAdjust = 0;
-
-				$scope.setTab = function(currentTab){
-					if($scope.currentTab !== currentTab){
-						$scope.currentTab = currentTab;
-					}
-				};
-
-				$scope.isCurrentTab = function(match){
-					return $scope.currentTab === match;
-				}
-			}
 		};
 	}])
 })();
