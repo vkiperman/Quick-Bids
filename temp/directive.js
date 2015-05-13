63dd8c9543c035dd6dd0b30f6fcc4a8d76e5b654
Vlad Kiperman <vkiperman@cars.com>
2015-05-13 16:30:49 -0500

    updated template

--- temp/directive.js
+++ temp/directive.js
@@ -9,20 +9,17 @@
 			// priority: 1,
 			// terminal: true,
 			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			// controller: function($scope, $element, $attrs, $transclude) {},
 			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 			// template: '',
 			// replace: true,
 			// transclude: true,
 			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			restrict: 'E',
+			restrict: 'E', // 'A', 'C'
 			templateUrl: '/includes/%TEMPLATENAME%',
+			controller: ['$scope', '$element', '$attrs', '$transclude', function($scope, $element, $attrs, $transclude) {
 
-			link: function(scope/*, element, attrs, transclude*/){
-
-			},
-
-			controller: function($scope){
+			}],
+			link: function(scope, element, attrs, ctrl){
 
 			}
 		};
