ddeffef076a8a94020e4da7637801b1b6de66690
Vlad Kiperman <vkiperman@cars.com>
2015-05-15 13:52:05 -0500

    refactored

--- temp/directive.js
+++ temp/directive.js
@@ -5,23 +5,33 @@
 	app.directive('%DIRECTIVENAME%', function(){
 
 		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			// template: '',
-			// replace: true,
-			// transclude: true,
+			// bindToController: true,
 			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			restrict: 'E', // 'A', 'C'
-			templateUrl: '/includes/%TEMPLATENAME%',
 			controller: ['$scope', '$element', '$attrs', '$transclude', function($scope, $element, $attrs, $transclude) {
 
 			}],
-			link: function(scope, element, attrs, ctrl){
+			// controllerAs: '%DIRECTIVENAME%Ctrl',
+			link: function(scope, element, attrs, %DIRECTIVENAME%Ctrl, transcludeFn){
 
-			}
+			},
+			// multiElement: true,
+			// name: '%DIRECTIVENAME%',
+			// priority: 0,
+			// replace: true, // [DEPRECATED!]
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check element and its parent, ^^ = check parent, ?^, ?^^
+			restrict: 'E', // 'A', 'C', 'M'
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change, 
+				// @ or @attr - bind a local scope property to the value of DOM attribute
+				// = or =attr - set up bi-directional binding between a local scope property and the parent scope property
+				// =? or =?attr in order to flag the property as optional. 
+				// =* or =*attr if you want to shallow watch for changes (i.e. $watchCollection instead of $watch)
+				// (=*? or =*?attr if the property is optional).
+				// & or &attr - provides a way to execute an expression in the context of the parent scope.
+			// template: '<div></div>',
+			templateUrl: '/includes/%TEMPLATENAME%',
+			// templateNamespace: 'html', // 'svg' or 'math'
+			// terminal: true,
+			// transclude: true
 		};
 
 	});
